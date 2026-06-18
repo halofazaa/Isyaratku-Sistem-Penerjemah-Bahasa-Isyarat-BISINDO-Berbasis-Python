b4b55438d542eddc4cf03d65929b9d32afd61f81
halofazaa <halo.fazaa@gmail.com>
2026-06-18 20:12:15 +0700

    Update train.py

--- train.py
+++ train.py
@@ -16,24 +16,21 @@ if not all_files:
 
 df_list = []
 for f in all_files:
-    # Membaca CSV tanpa header
     df = pd.read_csv(f, header=None)
     df_list.append(df)
 
 full_df = pd.concat(df_list, axis=0, ignore_index=True)
 
-# Memisahkan Label (Kolom 0) dan Fitur (Kolom 1 sampai 84)
 X = full_df.iloc[:, 1:].values
 y = full_df.iloc[:, 0].values
 
-# Cek konsistensi data
 print(f"Jumlah Sampel: {len(X)}")
 print(f"Jumlah Fitur: {X.shape[1]} (Harus 84)")
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
 print("2. Melatih Model...")
-model = RandomForestClassifier(n_estimators=100, random_state=42) # n_estimators standar agar file tidak berat
+model = RandomForestClassifier(n_estimators=100, random_state=42) 
 model.fit(X_train, y_train)
 
 acc = accuracy_score(y_test, model.predict(X_test))
